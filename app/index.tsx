5e73024bbc4ea8803087693826ba198b971d5239
Saleem3097 <md.saleem3097@gmail.com>
2025-10-31 21:26:52 +0530

    CHup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import Purple from "./Screens/Purple";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Purple />
+    </View>
+  );
+}
